5084060424d25caba92cad21fd873d29fb9a3334
Abylay <abilaisaiatuli@gmail.com>
2024-05-31 13:25:33 +0500

    Some change

--- client/src/main/java/kz/abl/ClientApp.java
+++ client/src/main/java/kz/abl/ClientApp.java
@@ -8,6 +8,6 @@ public class ClientApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "my not first commit");
     }
 }
