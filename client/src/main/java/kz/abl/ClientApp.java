10b4097e483a48bb115b71badf120d20be7a8fb7
Abylay <abilaisaiatuli@gmail.com>
2024-06-02 16:51:11 +0500

    HZ

--- client/src/main/java/kz/abl/ClientApp.java
+++ client/src/main/java/kz/abl/ClientApp.java
@@ -9,5 +9,8 @@ public class ClientApp
     public static void main( String[] args )
     {
         System.out.println( "my not first commit");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+        }
     }
 }
