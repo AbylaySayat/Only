4308302c42ef8b341a1c6d307c4ce2b706a32733
Abylay <abilaisaiatuli@gmail.com>
2024-06-02 17:13:45 +0500

    op1a

--- client/src/main/java/kz/abl/ClientApp.java
+++ client/src/main/java/kz/abl/ClientApp.java
@@ -8,6 +8,6 @@ public class ClientApp
 {
     public static void main( String[] args )
     {
-        System.out.println( "my not first commit");
+        System.out.println( "my first commit");
     }
 }
